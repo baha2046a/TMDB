f6b48a2c497b6ef5fa70ed9194ccc7e3c613e71d
baha2046a <80019240+baha2046a@users.noreply.github.com>
2021-03-24 02:01:14 +0900

    Remove file contain API key on public

--- app/src/main/java/com/example/tmdb/Common.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tmdb
-
-object Common {
-    const val baseUrl = "https://api.themoviedb.org/"
-    const val apiKey = "dbd468dd443c8122522d47916fbc3a59"
-
-    var language = "ja-JP" // Temp
-}
